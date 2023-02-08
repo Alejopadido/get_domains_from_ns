2d5c0fbfa62b4dcfa7eb2a449f54aeea3cb1f102
Alejandro Parrado <alejopadido13@gmail.com>
2023-02-07 22:39:24 -0500

    last updates

---
+++ instant_domains.py
@@ -0,0 +1,54 @@
+import json
+import requests
+
+def run():
+
+    # Welcome message
+    print("\nWelcome to Instant Domains Tool - By Alejandro P.D.\n")
+
+    # Ask user for information
+    print("Please enter the following information: \n")
+
+    name_server = input("Name server(ns): ")
+    api_key = input("API KEY: ")
+
+    # Make the GET request
+    response = requests.get(f"https://api.viewdns.info/reversens/?ns={name_server}&apikey={api_key}&output=json")
+
+    # Check if the request was successful
+    if response.status_code == 200:
+
+        with open("response.json", "w") as r:
+            r.write(response.content.decode("utf-8"))
+            
+    else:
+        print("Request failed with status code", response.status_code)
+
+
+    # Load JSON data from file
+    with open("response.json", "r") as f:
+        data = json.load(f)
+
+    # Extract the value of the 'domain' key
+    value = data["response"]
+
+    # Print number of domains found in the dictionary 'value'
+    domains_number = value["domain_count"]
+
+    # Save a list of key/value pairs with the domains
+    domains = value["domains"]
+
+    # Save each domain found in 'domains' list in a file
+    with open("domains.txt", "w") as o:
+
+        for item in domains:
+
+            domain = item["domain"]
+
+            o.write(str(domain) + "\n")
+    
+    print(f"\n{domains_number} domains successfully saved in domains.txt")
+
+
+if __name__ == '__main__':
+    run()
